459b7b949a4dc66e15938c8f426672890c8e3e22
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-07-23 15:34:57 -0300

    feat: Adiciona buscar ordem por id

--- src/main/java/com/metalflow/dao/OrdensDAO.java
+++ src/main/java/com/metalflow/dao/OrdensDAO.java
@@ -13,11 +13,10 @@ public class OrdensDAO {
     public List<OrdensProducao> buscarOrdens(/*ITENS PARA PAGINAÇÃO*/){
         List<OrdensProducao> listaOrdens = new ArrayList<>();
 
+
         /*
-
             LÓGICA DA PAGINAÇÃO
-
-         */
+        */
 
         String sql = "SELECT * FROM ordens_producao";
 
@@ -30,23 +29,26 @@ public class OrdensDAO {
                     OrdensProducao ordem = new OrdensProducao();
                     ordem.setId(rs.getInt("id"));
                     ordem.setNumero_op(rs.getString("numero_op"));
+                    ordem.setCodigo_produto(rs.getString("codigo_produto"));
                     ordem.setDescricao_produto(rs.getString("descricao_produto"));
                     ordem.setQuantidade_planejada(rs.getInt("quantidade_planejada"));
                     ordem.setSaldo_op(rs.getInt("saldo_op"));
                     ordem.setStatus_op(StatusOP.valueOf(rs.getString("status_op")));
-                    ordem.setCriado_em(rs.getTimestamp("criado_em").toLocalDateTime());
                     listaOrdens.add(ordem);
                 }
             }
         }catch(SQLException e){
-            throw new RuntimeException("Erro ao buscar ordens" + e.getMessage());
+            throw new RuntimeException("Erro ao buscar ordens" + e);
         }
 
         return listaOrdens;
     }
 
     public void inserirOrdem(OrdensProducao ordem){
-        String sql = "INSERT INTO ORDENS_PRODUCAO (numero_op, codigo_produto, descricao_produto, quantidade_planejada, saldo_op, criado_em) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO ORDENS_PRODUCAO (numero_op, codigo_produto, descricao_produto, quantidade_planejada, saldo_op) VALUES (?, ?, ?, ?, ?)";
+
+        ordem.setSaldo_op(ordem.getQuantidade_planejada());
+        ordem.setStatus_op(StatusOP.ABERTA);
 
         try(
                 Connection conn = ConnectionFactory.getConnection();
@@ -58,23 +60,49 @@ public class OrdensDAO {
             stmt.setString(3, ordem.getDescricao_produto());
             stmt.setInt(4, ordem.getQuantidade_planejada());
             stmt.setInt(5, ordem.getQuantidade_planejada());
-            stmt.setDate(6, Date.valueOf(ordem.getCriado_em().toLocalDate()));
 
             stmt.executeUpdate();
 
             try(ResultSet rs = stmt.getGeneratedKeys()){
                 if(rs.next()){
-                    ordem.setId(rs.getInt("id"));
+                    ordem.setId(rs.getInt(1));
                 }
             }
 
-            ordem.setSaldo_op(ordem.getSaldo_op());
-            ordem.setStatus_op(StatusOP.ABERTO);
-
         } catch (SQLException e) {
-            throw new RuntimeException("Erro ao cadastrar uma nova Ordem de Produção" + e.getMessage());
+            throw new RuntimeException("Erro ao cadastrar uma nova Ordem de Produção" + e);
         }
 
+    }
+
+    public OrdensProducao buscarOrdemPorId(int id){
+        String sql = "SELECT * FROM ORDENS_PRODUCAO WHERE id = ?";
+
+        try(
+                Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+                ){
+
+            stmt.setInt(1, id);
+
+            try(ResultSet rs = stmt.executeQuery()){
+                if (rs.next()) {
+                    OrdensProducao ordem = new OrdensProducao();
+                    ordem.setId(rs.getInt("id"));
+                    ordem.setNumero_op(rs.getString("numero_op"));
+                    ordem.setCodigo_produto(rs.getString("codigo_produto"));
+                    ordem.setDescricao_produto(rs.getString("descricao_produto"));
+                    ordem.setQuantidade_planejada(rs.getInt("quantidade_planejada"));
+                    ordem.setSaldo_op(rs.getInt("saldo_op"));
+                    ordem.setStatus_op(StatusOP.valueOf(rs.getString("status_op")));
+                    return ordem;
+                }
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException("Erro ao buscar ordem por Id" + e);
+        }
+        return null;
     }
 
 }
