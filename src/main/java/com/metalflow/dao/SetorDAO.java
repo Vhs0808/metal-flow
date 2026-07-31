a75730c4bcd96aa828a69d0ec80dd92feac664a9
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-07-30 21:55:33 -0300

    feat: Adiciona DAO de setor

---
+++ src/main/java/com/metalflow/dao/SetorDAO.java
@@ -0,0 +1,12 @@
+package com.metalflow.dao;
+
+import com.metalflow.util.ConnectionFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SetorDAO {
+}
