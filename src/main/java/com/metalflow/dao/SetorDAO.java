1ec39155499c4ff18c60ec7064699cf2aa44d988
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-07-30 21:55:54 -0300

    feat: Adiciona método buscarSetor

--- src/main/java/com/metalflow/dao/SetorDAO.java
+++ src/main/java/com/metalflow/dao/SetorDAO.java
@@ -9,4 +9,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SetorDAO {
+
+    public boolean buscarSetor(String codigo){
+
+        String sql = "SELECT codigo FROM SETORES WHERE codigo = ?";
+
+        try(
+                Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+                ){
+            stmt.setString(1, codigo);
+
+            try(ResultSet rs = stmt.executeQuery()){
+                if(rs.next()){
+                    return true;
+                }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Erro ao buscar setor ", e);
+        }
+        return false;
+    }
 }
