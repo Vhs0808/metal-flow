ebb1eb85ce64fc59004b67e7c9220624f656bbf7
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-08-01 20:05:23 -0300

    feat: Adiciona função de listar setores

--- src/main/java/com/metalflow/dao/SetorDAO.java
+++ src/main/java/com/metalflow/dao/SetorDAO.java
@@ -2,11 +2,11 @@ package com.metalflow.dao;
 
 import com.metalflow.util.ConnectionFactory;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 
 public class SetorDAO {
 
@@ -29,5 +29,29 @@ public class SetorDAO {
             throw new RuntimeException("Erro ao buscar setor ", e);
         }
         return false;
+    }
+
+    public List<Map<String, Object>> listaSetores(){
+        String sql="SELECT codigo FROM SETORES";
+
+        List<Map<String, Object>> setores = new ArrayList<>();
+
+        try(
+                Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()
+                ){
+
+            while(rs.next()){
+                Map<String, Object> setor = new LinkedHashMap<>();
+                setor.put("codigo", rs.getString("codigo"));
+                setores.add(setor);
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException("Erro ao buscar Setores", e);
+        }
+
+        return setores;
     }
 }
